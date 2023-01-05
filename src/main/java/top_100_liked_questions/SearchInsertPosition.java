fe8ae574838e7b7e844c474efc4d4ec83e830123
petko.lyutskanov <simona2000petko@abv.bg>
2023-01-05 17:21:27 +0200

    Climbing Stairs task

--- src/main/java/top_100_liked_questions/SearchInsertPosition.java
+++ src/main/java/top_100_liked_questions/SearchInsertPosition.java
@@ -24,7 +24,7 @@ public class SearchInsertPosition {
 
     public static void main(String[] args) {
         int[] nums = new int[] {1, 3, 5, 6};
-        int target = 7;
+        int target = 2;
 
         System.out.println(searchInsert(nums, target));
     }
