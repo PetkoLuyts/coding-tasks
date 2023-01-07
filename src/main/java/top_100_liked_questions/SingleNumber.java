7c7ce2bb99cca3260a04474f43cb42bddb90ad24
petko.lyutskanov <simona2000petko@abv.bg>
2023-01-07 17:44:59 +0200

    Single Number task

---
+++ src/main/java/top_100_liked_questions/SingleNumber.java
@@ -0,0 +1,19 @@
+package top_100_liked_questions;
+
+public class SingleNumber {
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1};
+
+        System.out.println(singleNumber(nums));
+    }
+}
