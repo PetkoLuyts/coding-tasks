8567964ec52c4882999d6d09252f19d200058a8c
petko.lyutskanov <simona2000petko@abv.bg>
2023-04-22 01:05:19 +0300

    Final Value After Operations task

---
+++ src/main/java/leetcode/arrays/FinalValueAfterOperations.java
@@ -0,0 +1,23 @@
+package leetcode.arrays;
+
+public class FinalValueAfterOperations {
+    public static int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+
+        for (String operation : operations) {
+            if (operation.contains("+")) {
+                x++;
+            } else x--;
+        }
+
+        return x;
+    }
+
+    public static void main(String[] args) {
+        String[] operations = new String[]{"X++","++X","--X","X--"};
+
+        int result = finalValueAfterOperations(operations);
+
+        System.out.println(result);
+    }
+}
