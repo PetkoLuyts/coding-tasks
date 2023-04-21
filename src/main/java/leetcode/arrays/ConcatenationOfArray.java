a2ba356cc34a76290d07b62b0e60fb69b58d253d
petko.lyutskanov <simona2000petko@abv.bg>
2023-04-21 21:42:18 +0300

    Concatenation Of Array task

---
+++ src/main/java/leetcode/arrays/ConcatenationOfArray.java
@@ -0,0 +1,28 @@
+package leetcode.arrays;
+
+import java.util.Arrays;
+
+public class ConcatenationOfArray {
+    public static int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i + n] = nums[i];
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 1};
+
+        int[] ans = getConcatenation(nums);
+
+        System.out.println(Arrays.toString(ans));
+    }
+}
