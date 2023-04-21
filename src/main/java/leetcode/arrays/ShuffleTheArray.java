cd45af847e7db0c88332ac4dce344ae571027101
petko.lyutskanov <simona2000petko@abv.bg>
2023-04-22 00:55:07 +0300

    Shuffle The Array task

---
+++ src/main/java/leetcode/arrays/ShuffleTheArray.java
@@ -0,0 +1,24 @@
+package leetcode.arrays;
+
+import java.util.Arrays;
+
+public class ShuffleTheArray {
+    public static int[] shuffle(int[] nums, int n) {
+        int[] res = new int[n * 2];
+
+        for (int i = 0, j = n, idx = 0; idx < res.length; i++, j++) {
+            res[idx++] = nums[i];
+            res[idx++] = nums[j];
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 5, 1, 3, 4, 7};
+
+        int[] res = shuffle(nums, nums.length / 2);
+
+        System.out.println(Arrays.toString(res));
+    }
+}
