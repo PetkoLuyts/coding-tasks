707aa090a903e51ce46b60748e37576152d81fac
petko.lyutskanov <simona2000petko@abv.bg>
2023-04-22 19:06:44 +0300

    Kids With Greatest Number Of Candies task

---
+++ src/main/java/leetcode/arrays/KidsWithGreatestNumberOfCandies.java
@@ -0,0 +1,35 @@
+package leetcode.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KidsWithGreatestNumberOfCandies {
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxNumberOfCandies = Arrays.stream(candies).max().getAsInt();
+        List<Boolean> result = new ArrayList<>();
+
+        for (int i = 0; i < candies.length; i++) {
+            candies[i] += extraCandies;
+
+            if (candies[i] >= maxNumberOfCandies) {
+                result.add(true);
+            } else result.add(false);
+        }
+
+        return result;
+
+        // Second two - line solution
+        // int max = Arrays.stream(candies).max().getAsInt();
+        // return Arrays.stream(candies).mapToObj(candy -> candy + extraCandies >= max).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        int[] candies = new int[]{2, 3, 5, 1, 3};
+        int extraCandies = 3;
+
+        List<Boolean> result = kidsWithCandies(candies, extraCandies);
+
+        System.out.println(Arrays.toString(result.toArray()));
+    }
+}
