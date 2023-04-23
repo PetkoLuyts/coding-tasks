d3c91afaf030c381dbb6e6e7131734efcb36f132
petko.lyutskanov <simona2000petko@abv.bg>
2023-04-23 09:48:26 +0300

    Running Sum Of 1d Array task

---
+++ src/main/java/leetcode/arrays/RunningSumOf1dArray.java
@@ -0,0 +1,25 @@
+package leetcode.arrays;
+
+import java.util.Arrays;
+
+public class RunningSumOf1dArray {
+    public static int[] runningSum(int[] nums) {
+        int currentSum = 0;
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            result[i] = currentSum;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, 1, 2, 10, 1};
+
+        int[] result = runningSum(nums);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
