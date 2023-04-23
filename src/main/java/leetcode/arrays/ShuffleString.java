e3aabea5a4e71d84d55ebbe4c93e9c861afc11ad
petko.lyutskanov <simona2000petko@abv.bg>
2023-04-23 12:44:28 +0300

    Shuffle String task

---
+++ src/main/java/leetcode/arrays/ShuffleString.java
@@ -0,0 +1,25 @@
+package leetcode.arrays;
+
+public class ShuffleString {
+    public static String restoreString(String s, int[] indices) {
+        char[] restoredCharArr = new char[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            int index = indices[i];
+            char ch = s.charAt(i);
+            restoredCharArr[index] = ch;
+        }
+
+        String restored = String.valueOf(restoredCharArr);
+        return restored;
+    }
+
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int[] indices = new int[]{4, 5, 6, 7, 0, 2, 1, 3};
+
+        String restoredString = restoreString(s, indices);
+
+        System.out.println(restoredString);
+    }
+}
