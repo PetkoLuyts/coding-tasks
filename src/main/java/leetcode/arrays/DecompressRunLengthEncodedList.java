bdae5c9c5d04ba7cc89414aa3ccda0b90334cbce
petko.lyutskanov <simona2000petko@abv.bg>
2023-04-23 11:58:20 +0300

    Decompress Run Length Encoded List task

---
+++ src/main/java/leetcode/arrays/DecompressRunLengthEncodedList.java
@@ -0,0 +1,29 @@
+package leetcode.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DecompressRunLengthEncodedList {
+    public static int[] decompressRLEList(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i += 2) {
+            int frequency = nums[i];
+            int value = nums[i + 1];
+
+            for (int j = 0; j < frequency; j++) {
+                list.add(value);
+            }
+        }
+
+        return list.stream().mapToInt(num -> num).toArray();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4};
+
+        int[] result = decompressRLEList(nums);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
