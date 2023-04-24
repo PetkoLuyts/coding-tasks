f31a268642f0f8ab2187c851267c780e50ae7239
petko.lyutskanov <simona2000petko@abv.bg>
2023-04-24 12:10:26 +0300

    Count Items Matching Rule task

---
+++ src/main/java/leetcode/arrays/CountItemsMatchingRule.java
@@ -0,0 +1,47 @@
+package leetcode.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountItemsMatchingRule {
+    public static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int matches = 0;
+
+        for (List<String> item : items) {
+            if (ruleKey.equals("type") && item.get(0).equals(ruleValue)) matches++;
+            if (ruleKey.equals("color") && item.get(1).equals(ruleValue)) matches++;
+            if (ruleKey.equals("name") && item.get(2).equals(ruleValue)) matches++;
+        }
+
+        return matches;
+    }
+
+    public static void main(String[] args) {
+        List<List<String>> items = new ArrayList<>();
+        List<String> innerList1 = new ArrayList<>();
+        List<String> innerList2 = new ArrayList<>();
+        List<String> innerList3 = new ArrayList<>();
+        String ruleKey = "color";
+        String ruleValue = "silver";
+
+        innerList1.add("phone");
+        innerList1.add("blue");
+        innerList1.add("pixel");
+
+        innerList2.add("computer");
+        innerList2.add("silver");
+        innerList2.add("lenovo");
+
+        innerList3.add("phone");
+        innerList3.add("gold");
+        innerList3.add("iphone");
+
+        items.add(innerList1);
+        items.add(innerList2);
+        items.add(innerList3);
+
+        int matches = countMatches(items, ruleKey, ruleValue);
+
+        System.out.println(matches);
+    }
+}
