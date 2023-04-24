31a83606e1dece7db73845275eda98353b536d63
petko.lyutskanov <simona2000petko@abv.bg>
2023-04-24 15:11:25 +0300

    Number Of Arithmetic Triplets task - important

---
+++ src/main/java/leetcode/arrays/NumberOfArithmeticTriplets.java
@@ -0,0 +1,28 @@
+package leetcode.arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NumberOfArithmeticTriplets {
+    public static int arithmeticTriplets(int[] nums, int diff) {
+        int count = 0;
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num - diff) && seen.contains(num - diff * 2)) {
+                ++count;
+            }
+            seen.add(num);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 1, 4, 6, 7, 10};
+        int diff = 3;
+
+        int numberOfTriplets = arithmeticTriplets(nums, diff);
+
+        System.out.println(numberOfTriplets);
+    }
+}
