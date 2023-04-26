3b0fb43d24b73a3a3c3ad66635b9c6719fc8173f
petko.lyutskanov <simona2000petko@abv.bg>
2023-04-26 12:40:48 +0300

    Fix Merge Two Strings task

---
+++ src/main/java/leetcode/arrays/ContainsDuplicate.java
@@ -0,0 +1,25 @@
+package leetcode.arrays;
+
+import java.util.HashMap;
+
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, 1);
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 1};
+
+        System.out.println(containsDuplicate(nums));
+    }
+}
--- src/main/java/leetcode/strings/MergeTwoStrings.java
+++ src/main/java/leetcode/strings/MergeTwoStrings.java
@@ -2,22 +2,22 @@ package leetcode.strings;
 
 public class MergeTwoStrings {
     public static String mergeTwoStrings(String arr1, String arr2) {
-        int largestArr = Math.max(arr1.length(), arr2.length());
+        int minArrLength = Math.min(arr1.length(), arr2.length());
         int difference = Math.abs(arr1.length() - arr2.length());
         String result = "";
 
-        if (difference == 0) {
-            for (int i = 0; i < largestArr; i++) {
-                result += arr1.charAt(i);
-                result += arr2.charAt(i);
-            }
-        } else if (arr1.length() > arr2.length()) {
+        for (int i = 0; i < minArrLength; i++) {
+            result += arr1.charAt(i);
+            result += arr2.charAt(i);
+        }
+
+        if (difference != 0 && arr1.length() > arr2.length()) {
             for (int i = arr2.length(); i < arr1.length(); i++) {
-                result += i;
+                result += arr1.charAt(i);
             }
         } else {
             for (int i = arr1.length(); i < arr2.length(); i++) {
-                result += i;
+                result += arr2.charAt(i);
             }
         }
 
