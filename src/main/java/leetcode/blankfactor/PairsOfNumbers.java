f9de3e2722222a900691aef45cbe3fd1b9bf5cfe
petko.lyutskanov <simona2000petko@abv.bg>
2023-04-28 17:42:00 +0300

    Pairs Of Numbers task another approach

--- src/main/java/leetcode/blankfactor/PairsOfNumbers.java
+++ src/main/java/leetcode/blankfactor/PairsOfNumbers.java
@@ -1,32 +1,27 @@
 package leetcode.blankfactor;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class PairsOfNumbers {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3, 4, 5};
+        List<Integer> numsAsList = Arrays.stream(nums).boxed().toList();
         int target = 7;
 
         HashMap<Integer, Integer> numsMap = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j < nums.length; j++) {
-                if (!numsMap.containsKey(nums[i])) {
-                    numsMap.put(nums[i], nums[i]);
-                }
+        for (int i = 0; i < numsAsList.size(); i++) {
+            int difference = target - numsAsList.get(i);
 
-                if (target - numsMap.get(nums[i]) == nums[j]) {
-                    numsMap.put(nums[i], nums[j]);
+            if (numsAsList.contains(difference)) {
+                if (!numsMap.containsKey(difference)) {
+                    numsMap.put(numsAsList.get(i), difference);
                 }
             }
         }
 
         for (Map.Entry<Integer, Integer> set : numsMap.entrySet()) {
-            if (set.getKey() != set.getValue()) {
-                System.out.print("(" + set.getKey() + ", "
-                        + set.getValue() + ")");
-            }
+            System.out.print("(" + set.getKey() + ", "
+                    + set.getValue() + ")");
         }
     }
 }
