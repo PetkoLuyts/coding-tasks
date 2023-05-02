6677bf02c15071ba33066007a527647e4436808d
petko.lyutskanov <simona2000petko@abv.bg>
2023-05-02 15:55:54 +0300

    Car task

---
+++ src/main/java/leetcode/blankfactor/oop/classes_and_objects/car_task/Tyre.java
@@ -0,0 +1,30 @@
+package leetcode.blankfactor.oop.classes_and_objects.car_task;
+
+public class Tyre {
+    private double pressure;
+    private int age;
+
+    public Tyre() {
+    }
+
+    public Tyre(double pressure, int age) {
+        this.pressure = pressure;
+        this.age = age;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(double pressure) {
+        this.pressure = pressure;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
