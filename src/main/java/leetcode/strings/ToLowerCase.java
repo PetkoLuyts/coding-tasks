46bd4a1cc2235d373272b6a1e67c223340253dae
petko.lyutskanov <simona2000petko@abv.bg>
2023-05-13 11:49:42 +0300

    To Lower Case task

---
+++ src/main/java/leetcode/strings/ToLowerCase.java
@@ -0,0 +1,13 @@
+package leetcode.strings;
+
+public class ToLowerCase {
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        String s = "LOVELY";
+
+        System.out.println(toLowerCase(s));
+    }
+}
