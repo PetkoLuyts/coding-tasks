59e836837cb10c586d05f06f85e7bebf46741482
petko.lyutskanov <simona2000petko@abv.bg>
2023-05-14 11:31:15 +0300

    Next Greater Element I task

---
+++ src/main/java/leetcode/hashtable/easy/NextGreaterElementI.java
@@ -0,0 +1,32 @@
+package leetcode.hashtable.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class NextGreaterElementI {
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums2) {
+            while (!stack.isEmpty() && num > stack.peek())
+                map.put(stack.pop(), num);
+            stack.push(num);
+        }
+
+        int i = 0;
+        for (int num : nums1)
+            result[i++] = map.getOrDefault(num, -1);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1, 3, 5, 2, 4};
+        int[] nums2 = new int[]{6, 5, 4, 3, 2, 1, 7};
+
+        System.out.println(Arrays.toString(nextGreaterElement(nums1, nums2)));
+    }
+}
