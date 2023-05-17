de51576502e1436e7867bd3645155719088d2ee9
petko.lyutskanov <simona2000petko@abv.bg>
2023-05-17 09:54:44 +0300

    Create neetcode tasks package

---
+++ src/main/java/neetcode/arrays_and_hashing/TopKFrequentElements.java
@@ -0,0 +1,41 @@
+package neetcode.arrays_and_hashing;
+
+import java.util.*;
+
+public class TopKFrequentElements {
+    public static int[] topKFrequent(int[] nums, int k) {
+        List<Integer>[] bucket = new List[nums.length + 1];
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        for (int n : nums) {
+            frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+        }
+
+        for (int key : frequencyMap.keySet()) {
+            int frequency = frequencyMap.get(key);
+
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+
+            bucket[frequency].add(key);
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+            if (bucket[pos] != null) {
+                res.addAll(bucket[pos]);
+            }
+        }
+
+        return res.stream().mapToInt(i -> i).toArray();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 1, 2, 2, 3};
+        int k = 2;
+
+        System.out.println(Arrays.toString(topKFrequent(nums, k)));
+    }
+}
---
+++ src/main/java/neetcode/arrays_and_hashing/ValidAnagram.java
@@ -0,0 +1,39 @@
+package neetcode.arrays_and_hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        if(s.equals(t)) return true;
+
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            sMap.merge(s.charAt(i), 1, Integer::sum);
+            tMap.merge(t.charAt(i), 1, Integer::sum);
+        }
+
+        for(Character c : sMap.keySet()) {
+            if(!sMap.get(c).equals(tMap.get(c))) return false;
+        }
+
+        return true;
+
+//        Second solution
+//        int[] alphabet = new int[26];
+//        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+//        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+//        for (int i : alphabet) if (i != 0) return false;
+//        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+
+        System.out.println(isAnagram(s, t));
+    }
+}
