db5f1fe179c981b5d935bba541be7611afb1b29d
PetkoLyuts <simona2000petko@abv.bg>
2023-07-08 19:23:45 +0300

    Managers With At Least 5 Direct Reports sql task

---
+++ src/main/java/sql/leetcode/easy/managers_with_at_least_5_direct_reports.sql
@@ -0,0 +1,14 @@
+Create table If Not Exists Employee (id int, name varchar(255), department varchar(255), managerId int);
+Truncate table Employee;
+insert into Employee (id, name, department, managerId) values ('101', 'John', 'A', 0);
+insert into Employee (id, name, department, managerId) values ('102', 'Dan', 'A', '101');
+insert into Employee (id, name, department, managerId) values ('103', 'James', 'A', '101');
+insert into Employee (id, name, department, managerId) values ('104', 'Amy', 'A', '101');
+insert into Employee (id, name, department, managerId) values ('105', 'Anne', 'A', '101');
+insert into Employee (id, name, department, managerId) values ('106', 'Ron', 'B', '101');
+
+SELECT e2.name FROM Employee AS e
+JOIN Employee AS e2
+ON e2.id = e.managerId
+GROUP BY e2.id, e2.name
+HAVING COUNT(*) >= 5;
