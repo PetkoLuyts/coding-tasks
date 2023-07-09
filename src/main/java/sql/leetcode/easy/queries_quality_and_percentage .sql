e767424219718130d04e85fd69e3b4fd32d9d1fb
PetkoLyuts <simona2000petko@abv.bg>
2023-07-09 12:45:30 +0300

    Queries Quality and Percentage sql task

---
+++ src/main/java/sql/leetcode/easy/queries_quality_and_percentage .sql
@@ -0,0 +1,14 @@
+Create table If Not Exists Queries (query_name varchar(30), result varchar(50), position int, rating int);
+Truncate table Queries;
+insert into Queries (query_name, result, position, rating) values ('Dog', 'Golden Retriever', '1', '5');
+insert into Queries (query_name, result, position, rating) values ('Dog', 'German Shepherd', '2', '5');
+insert into Queries (query_name, result, position, rating) values ('Dog', 'Mule', '200', '1');
+insert into Queries (query_name, result, position, rating) values ('Cat', 'Shirazi', '5', '2');
+insert into Queries (query_name, result, position, rating) values ('Cat', 'Siamese', '3', '3');
+insert into Queries (query_name, result, position, rating) values ('Cat', 'Sphynx', '7', '4');
+
+SELECT q.query_name,
+	   ROUND(AVG(q.rating / position), 2) AS quality,
+	   ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) /  COUNT(query_name) * 100, 2) AS poor_query_percentage
+FROM Queries AS q
+GROUP BY q.query_name;
