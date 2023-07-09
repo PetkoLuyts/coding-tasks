b768b4fb7bb96e6ba665493373432fe6ec436f6d
PetkoLyuts <simona2000petko@abv.bg>
2023-07-09 18:05:41 +0300

    Game Play Analysis IV sql task

---
+++ src/main/java/sql/leetcode/medium/game_play_analysis_4.sql
@@ -0,0 +1,12 @@
+Create table If Not Exists Activity (player_id int, device_id int, event_date date, games_played int);
+Truncate table Activity;
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-03-01', '5');
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-03-02', '6');
+insert into Activity (player_id, device_id, event_date, games_played) values ('2', '3', '2017-06-25', '1');
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '1', '2016-03-02', '0');
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '4', '2018-07-03', '5');
+
+SELECT ROUND(COUNT(t2.player_id)/COUNT(t1.player_id),2) AS fraction
+FROM
+(SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id) t1 LEFT JOIN Activity t2
+ON t1.player_id = t2.player_id AND DATEDIFF(t2.event_date , t1.first_login) = 1;
